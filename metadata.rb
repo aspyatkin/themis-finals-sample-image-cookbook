92dd9b5961ad123fa59e52ba7342e51479ec29a0
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2017-04-22 17:26:27 +0400

    Update dependencies

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'themis-finals-sample-image'
 description 'Installs and configures Themis Finals sample image'
-version '1.1.0'
+version '1.1.1'
 
 recipe 'themis-finals-sample-image', 'Installs and configures Themis Finals sample image'
-depends 'poise-python', '~> 1.4.0'
+depends 'poise-python', '~> 1.6.0'
